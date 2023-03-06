f491dc49b38ebb2a23d793588661ca4e9a5374c5
João Gabriel Moreira Bossatto Daré <79703333+Joaogmbd@users.noreply.github.com>
2023-03-06 09:48:15 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+var appRoutes = require('./routes/app');
+
+var app = express();
+mongoose.connect('mongodb://localhost:27017/node-mongoose-ativ')
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use('/', appRoutes);
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+    next();
+});
+
+
+module.exports = app; 
